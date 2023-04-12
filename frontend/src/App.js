4f6be530be82e17d811eda3cae66f2e9484aaba1
Gopal naik guguloth <gugulothgopalnaik@gmail.com>
2023-04-12 11:29:41 +0530

    setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-9xl font-bold underline">APP</h1>
+    </div>
+  )
+}
+
+export default App
